e2a8eb329550276aa1e463de659c6d5d7516880f
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-05-07 20:27:48 +0300

    added NavBar for navigation to different pages, cocktails and cocktails list to fetch and display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import UserProfile from './pages/UserProfile';
+import CocktailList from './pages/CocktailList';
+import CreateCocktail from './pages/CreateCocktail';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <div className="dashboard">
+          <NavBar />
+          <div className="content">
+            <HomePage />
+          </div>
+        </div>
+      ),
+    },
+    {
+      path: '/cocktail',
+      element: (
+        <div className="dashboard">
+          <NavBar />
+          <div className="content">
+            <CocktailList />
+          </div>
+        </div>
+      ),
+    },
+    {
+      path: '/create',
+      element: (
+        <div className="dashboard">
+          <NavBar />
+          <div className="content">
+            <CreateCocktail />
+          </div>
+        </div>
+      ),
+    },
+    {
+      path: '/profile',
+      element: (
+        <div className="dashboard">
+          <NavBar />
+          <div className="content">
+            <UserProfile />
+          </div>
+        </div>
+      ),
+    },
+  ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/CocktailList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cocktails from '../components/Cocktails'
+
+function CocktailList() {
+  return (
+    <div>
+        <Cocktails/>
+    </div>
+  )
+}
+
+export default CocktailList
