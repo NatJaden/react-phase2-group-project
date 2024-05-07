8f4cdfd312d8677d97128768fef378d6497536d5
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-05-07 12:33:22 +0300

    coded cocktail list

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,26 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import CocktailCard from './CocktailCard';
+import { Link, useParams } from 'react-router-dom';
 
-function CocktailList() {
-  const [cocktails, setCocktails] = useState([]);
-
-  useEffect(() => {
-    fetch('https://www.thecocktaildb.com/api.php')
-      .then(response => response.json())
-      .then(data => setCocktails(data))
-      .catch(error => console.log(error));
-  }, []);
-
+function CocktailCard({ id, name, ingredients, image }) {
   return (
-    <div>
-      <h2>Cocktail List</h2>
-      <div className="card-container">
-        {cocktails.map(cocktail => (
-          <CocktailCard key={cocktail.id} {...cocktail} />
-        ))}
-      </div>
+    <div className="card">
+      <h3>{name}</h3>
+      <p>Ingredients: {ingredients.join(', ')}</p>
+      <img src={image} alt="Cocktail" />
+      <Link to={`/cocktail/${id}`}>Details</Link>
     </div>
   );
 }
 
-export default CocktailList;
+export function CocktailDetails() {
+  const { id } = useParams();
+  const [cocktail, setCocktail] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then(response => response.json())
+      .then(data => setCocktail(data.drinks[0]))
+      .catch(error => console.log(error));
+  }, [id]);
+
+  if (!cocktail) return <div>Loading...</div>;
+
+  const ingredients = Object.keys(cocktail)
+    .filter(key => key.startsWith('strIngredient') && cocktail[key])
+    .map(key => cocktail[key]);
+
+  return (
+    <div>
+      <h2>{cocktail.strDrink}</h2>
+      <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+      <p>Ingredients: {ingredients.join(', ')}</p>
+    </div>
+  );
+}
+
+export default CocktailCard;
